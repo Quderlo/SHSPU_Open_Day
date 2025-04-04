3b1dfffefa27d510d6da2937e085e980f786eb44
Quderlo <lev.pedoshenko@gmail.com>
2025-04-05 01:59:05 +0500

    Final

--- src/apps/user/urls.py
+++ src/apps/user/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LogoutView
 from apps.user.views.registration import RegistrationView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', LoginView.as_view(), name='login-tmp'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout-tmp'),
+    path('registration/', RegistrationView.as_view(), name='registration-tmp'),
 ]
